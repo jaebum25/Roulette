fe3a2a2327ba91725208d64385ae4a7dc6f9e13e
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2022-11-09 16:00:26 -0500

    Day 3 game works, work on user exp

--- js/script.js
+++ js/script.js
@@ -1,76 +1,139 @@
 /*----- constants -----*/
-const winMultiplier = [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 2, 12, 2]
+const winMultiplier = [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 2, 12, 2,];
 
 /*----- app's state (variables) -----*/
 
-let spinHistory = []
-let winNum
-let wager
-let betamtEl
-let betbalEl = 100
+let spinHistory = [];
+let winNum = 0;
+let wager = 0;
+let betamtEl = 0;
+let bankBal = 100;
+let chipVal = 0
 // this will change depending on what is bet
-let board = [0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5]
+let board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 // This will assign a value of 1 to the number and 1 to the color
-let winner = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] 
-
-// this multiplies the 3 arrays into 1 array as a product
-totalWinArray = []
-for (i = 0; i < winMultiplier.length; i++) {
-    totalWinArray[i] = winner[i] * winMultiplier[i] * board[i]
-}
-
-// this takes the new 1 array product, and sums each element to 1 value
-let totalWinSum = 0
-for(i = 0; i < totalWinArray.length; i++) {
-    totalWinSum += totalWinArray[i];
-}
-// this would be the total $ you won based on bet x winning number x win multipliers
-console.log(`total winnings: ${totalWinSum}`)
+let winner = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 /*----- cached element references -----*/
 
-const cellsEls = document.querySelectorAll('.cell')
-const spinBtn = document.querySelector('#spin')
-const betlogEl = document.getElementById('betlog')
-const wheelEl = document.getElementById('wheel')
-const betAmtEl = document.getElementById('betamt')
-const betBalEl = document.getElementById('betbal')
+const cellsEls = document.querySelectorAll(".cell");
+const spinBtn = document.querySelector("#spin");
+const betlogEl = document.getElementById("betlog");
+const wheelEl = document.getElementById("wheel");
+const betAmtEl = document.getElementById("betamt");
+const betBalEl = document.getElementById("betbal");
+const betchipsEl = document.querySelectorAll(".betchips");
+let chip5El = document.getElementById("chip5");
+let chip10El = document.getElementById("chip10");
+let chip25El = document.getElementById("chip25");
+let chip50El = document.getElementById("chip50");
+let chip100El = document.getElementById("chip100");
 
 /*----- event listeners -----*/
 
-spinBtn.addEventListener('click', handleSpin)
+spinBtn.addEventListener("click", handleSpin);
+
+// chip5El.addEventListener('click', handleChip)
+// chip10El.addEventListener('click', handleChip)
+// chip25El.addEventListener('click', handleChip)
+// chip50El.addEventListener('click', handleChip)
+// chip100El.addEventListener('click', handleChip)
 // cellsEls.addEventListener('click', handleBet)
 
-
 /*----- functions -----*/
+
 function handleSpin() {
-    winNum = spinWheel()
-    spinHistory.unshift(winNum)
-    console.log(winNum)
-    betlogEl.innerText = spinHistory.slice(0, 15)
-    wheelEl.innerText = winNum
-    if (winNum === 0) {
-        wheelEl.style.backgroundColor = 'green';
-    } else {
-        wheelEl.style.backgroundColor = winNum % 2 !== 1 ? "black" : "red";
-    }
-    // sets betamt text to $0 after each click
-    betAmtEl.innerText = `BET $${0}`
-    wager = 0
+  winNum = spinWheel();
+  logSpinHistory();
+  if (winNum === 0) {
+    wheelEl.style.backgroundColor = "green";
+  } else {
+    wheelEl.style.backgroundColor = winNum % 2 !== 1 ? "black" : "red";
+  }
+  // sets wager text to $0 after each click
+  betAmtEl.innerText = `BET $${0}`;
+  wager = 0;
+  updateWinner();
+  console.log(winner);
+  calcWinnings();
+  // Need to have winnings multiplied here before we reset our board
+  resetWinner();
+  resetBoard();
+  console.log(winner);
 }
 
+// This is good and complete
+function logSpinHistory() {
+  wheelEl.innerText = winNum;
+  spinHistory.unshift(winNum);
+  console.log(winNum);
+  betlogEl.innerText = spinHistory.slice(0, 15);
+}
+
+// This is good and complete
 function spinWheel() {
-    return Math.floor(Math.random() * 12)
+  return Math.floor(Math.random() * 13);
 }
 
-cellsEls.forEach(cell => {
-    cell.addEventListener('click', function handleClick(evt) {
-        wager++
-        console.log(evt.target.id)
-        console.log(`wager = ${wager}`)
-// sets the wager to X based on clicking
-        betAmtEl.innerText = `BET $${wager}`
-// sets betbal to = the difference between the curr betbal and the curr wager before spininng
-        betBalEl.innerText = `BAL $${betbalEl - wager}`
-    })
-})
+function updateBoard() {
+  board[evt.target.id] = 7;
+}
+
+// This is good and complete
+function resetBoard() {
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+
+// This is good and complete
+function updateWinner() {
+  if (winNum === 0) {
+    winner[13] = 1;
+  } else {
+    winner[winNum - 1] = 1;
+    winNum % 2 !== 1 ? (winner[12] = 1) : (winner[14] = 1);
+  }
+}
+
+// This is good and complete
+function resetWinner() {
+  // winner[winNum] = 0
+  winner = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+
+cellsEls.forEach((cell) => {
+  cell.addEventListener("click", function handleClick(evt) {
+      // total my wager, and have a separte var for chipVal.
+    wager += chipVal
+    bankBal -= chipVal
+    console.log(evt.target.id);
+    // console.log(`wager = ${wager}`)
+    // sets the wager to X based on clicking
+    betAmtEl.innerText = `BET $${wager}`;
+    // sets betbal to = the differencse between the curr betbal and the curr wager before spininng
+    betBalEl.innerText = `BAL $${bankBal}`;
+    // updateBoard()
+    board[evt.target.id] += chipVal;
+    console.log(board);
+  });
+});
+//This is good and complete
+function calcWinnings() {
+  let totalWinArray = [];
+  // this multiplies the 3 arrays into 1 array as a product
+  for (i = 0; i < winMultiplier.length; i++) {
+    totalWinArray[i] = winner[i] * winMultiplier[i] * board[i];
+  }
+  let totalWinSum = 0;
+  // this takes the new 1 array product, and sums each element to 1 value
+  for (i = 0; i < totalWinArray.length; i++) {
+    totalWinSum += totalWinArray[i];
+  }
+  // this would be the total $ you won based on bet x winning number x win multipliers
+  console.log(`total winnings: ${totalWinSum}`);
+}
+
+function storeVal(v) {
+    chipVal = parseInt(v);
+    // this sets global chipVal
+    console.log(chipVal)
+}
