a66ebdad520432201620cf6a04e031df7f650d34
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2022-11-10 17:01:51 -0500

    finishing touches on game

--- js/script.js
+++ js/script.js
@@ -1,78 +1,91 @@
 /*----- constants -----*/
-const winMultiplier = [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 0, 12, 4,];
+const winMultiplier = [
+  12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 0, 12, 4,
+];
 
 /*----- app's state (variables) -----*/
 
 let spinHistory = [];
-let winNum = 0
-let wager = 0;
+let winNum = 0;
+let wager = 0.01;
 let bankBal = 100;
-let chipVal = 0
-let totalWinSum = 0
+let chipVal = 0;
+let totalWinSum = 0;
 let board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let winner = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 /*----- cached element references -----*/
 
 let cellsEls = document.querySelectorAll(".cell");
-let spinBtn = document.querySelector("#spin");
+let spinBtnEl = document.querySelector("#spin");
 let betlogEl = document.getElementById("betlog");
 let wheelEl = document.getElementById("wheel");
 let betAmtEl = document.getElementById("betamt");
 let betBalEl = document.getElementById("betbal");
-let winAlertEl = document.querySelector(".win-alert")
+let winAlertEl = document.querySelector(".win-alert");
 
 /*----- event listeners -----*/
 
-spinBtn.addEventListener("click", handleSpin);
-
+spinBtnEl.addEventListener("click", handleSpin);
 
 /*----- functions -----*/
 
-function handleSpin() {
-  winNum = spinWheel();
-  logSpinHistory();
-  styleWheel();
-  resetWager();
-  updateWinner();
-  calcWinnings();
-  resetWinner();
-  resetBoard();
-}
-
-// This is good and complete
-function spinWheel() {
-    return Math.floor(Math.random() * 13);
-}
-
-// This is good and complete
-function logSpinHistory() {
-    wheelEl.innerText = winNum;
-    spinHistory.unshift(winNum);
-    betlogEl.innerText = spinHistory.slice(0, 12);
-    // informational console log
-    console.log(`lucky number: ${winNum}`);
-}
-
-// This is good and complete
-function styleWheel() {
-    // styling the wheel color
-    if (winNum === 0) {
-        wheelEl.style.backgroundColor = "#1dff00";
-    } else {
-        wheelEl.style.backgroundColor = winNum % 2 !== 1 ? "black" : "#ff6c00";
-        wheelEl.style.color = winNum % 2 !== 1 ? "#ff6c00" : "black";
+cellsEls.forEach((cell) => {
+  cell.addEventListener("click", function clickCell(evt) {
+    if (bankBal > 0 && wager >= 0) {
+      if (chipVal > bankBal) {
+        chipVal = 0;
+      }
+      wager += chipVal;
+      bankBal -= chipVal;
+      betAmtEl.innerText = `BET $${Math.floor(wager)}`;
+      betBalEl.innerText = `BAL $${bankBal}`;
+      board[evt.target.id] += chipVal;
     }
+  });
+});
+
+function storeVal(v) {
+  chipVal = parseInt(v);
 }
 
-// This is good and complete
+function handleSpin() {
+  if (bankBal >= 0 && wager >= 0) {
+    winNum = spinWheel();
+    logSpinHistory();
+    styleWheel();
+    resetWager();
+    updateWinner();
+    calcWinnings();
+    resetWinner();
+    resetBoard();
+  }
+}
+
+function spinWheel() {
+  return Math.floor(Math.random() * 13);
+}
+
+function logSpinHistory() {
+  wheelEl.innerText = winNum;
+  spinHistory.unshift(winNum);
+  betlogEl.innerText = spinHistory.slice(0, 12);
+}
+
+function styleWheel() {
+  if (winNum === 0) {
+    wheelEl.style.backgroundColor = "#1dff00";
+  } else {
+    wheelEl.style.backgroundColor = winNum % 2 !== 1 ? "black" : "#ff6c00";
+    wheelEl.style.color = winNum % 2 !== 1 ? "#ff6c00" : "black";
+  }
+}
+
 function resetWager() {
-    // sets wager text to $0 after each click
-    betAmtEl.innerText = `BET $${0}`;
-    wager = 0;
+  betAmtEl.innerText = `BET $${0}`;
+  wager = 0;
 }
 
-// This is good and complete
 function updateWinner() {
   if (winNum === 0) {
     winner[13] = 1;
@@ -80,62 +93,40 @@ function updateWinner() {
     winner[winNum - 1] = 1;
     winNum % 2 !== 1 ? (winner[12] = 1) : (winner[14] = 1);
   }
-  console.log(winner)
 }
 
-//This is good and complete
 function calcWinnings() {
   let totalWinArray = [];
-  // this multiplies the 3 arrays into 1 array as a product
   for (i = 0; i < winMultiplier.length; i++) {
     totalWinArray[i] = winner[i] * winMultiplier[i] * board[i];
   }
-  let totalWinSum = 0
-  // this takes the new 1 array product, and sums each element to 1 value
+  let totalWinSum = 0;
   for (i = 0; i < totalWinArray.length; i++) {
     totalWinSum += totalWinArray[i];
   }
   if (totalWinSum > 0) {
-    winAlertEl.textContent = `Here's a treat! $${totalWinSum}`
-    winAlertEl.style.color = "#1dff00"
+    winAlertEl.textContent = `Here's a treat! $${totalWinSum}`;
+    winAlertEl.style.color = "#1dff00";
+  } else if (totalWinSum === 0 && wager === 0 && bankBal === 0) {
+    winAlertEl.textContent = `Has anybody ever told you, you have a SERIOUS IMPULSE CONTROL PROBLEM?!`;
+    winAlertEl.style.color = "#9700f8";
+    winAlertEl.style.backgroundColor = "#1dff00";
+    wheelEl.innerText = "¿";
+    wheelEl.style.color = "black";
+    wheelEl.style.backgroundColor = "#1dff00";
+    spinBtnEl.innerHTML = "You lose!";
   } else {
-    winAlertEl.textContent = `Tricked ya! haha :P`
-    winAlertEl.style.color = "rgb(233, 0, 0)"
+    winAlertEl.textContent = `Tricked ya! haha :P`;
+    winAlertEl.style.color = "rgb(233, 0, 0)";
   }
-  console.log(totalWinSum)
-  console.log(totalWinArray)
-  bankBal = bankBal + totalWinSum
+  bankBal = bankBal + totalWinSum;
   betBalEl.innerText = `BAL $${bankBal}`;
-  // this would be the total $ you won based on bet x winning number x win multipliers
-  //informational console log
-  console.log(`total winnings: ${totalWinSum}`);
 }
 
-// This is good and complete
 function resetWinner() {
   winner = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
 
-// This is good and complete
 function resetBoard() {
   board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-}
-
-// This is good and complete
-cellsEls.forEach((cell) => {
-  cell.addEventListener("click", function handleClick(evt) {
-    wager += chipVal
-    bankBal -= chipVal
-    betAmtEl.innerText = `BET $${wager}`;
-    betBalEl.innerText = `BAL $${bankBal}`;
-    board[evt.target.id] += chipVal;
-    console.log(board);
-  });
-});
-
-// This is good and complete
-function storeVal(v) {
-    chipVal = parseInt(v);
-    // this sets global chipVal
-    console.log(chipVal)
 }
